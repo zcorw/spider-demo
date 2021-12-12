e6b21149b7b3d6cae1cc6b53ac22aa151e214ff8
zhangzw <zhzhwwt@qq.com>
2021-12-12 17:20:11 +0800

    完成

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const fs = require('fs');
+const path = require('path');
 const Crawler = require("crawler");
 const YAML = require('yaml');
 const targetYml = fs.readFileSync('./target.yml', 'utf8');
 const targetConf = YAML.parse(targetYml);
 let tmpList = [];
+// 从配置中获取到对应剧的区块
+const getConfig = (name) => {
+  return targetConf.list.find(item => item.name === name);
+}
 // 入口页面获取目标列表
 const enterProcessor = ($) => {
   const listDom = $('.post_td').first().find('table').first().find('tr');
   const list = [];
-  listDom.map(function(index) {
+  listDom.map(function (index) {
     if (index === 0) return;
     const item = [];
-    $(this).find('td').map(function(j) {
+    $(this).find('td').map(function (j) {
       if (j === 0) {
         item.push(+$(this).text());
       } else if (j === 1) {
@@ -28,7 +33,7 @@ const enterProcessor = ($) => {
     const group = item[3];
     const result = title.match(/^\[(.+)\]\[(.+)\](\[更新至\s?[0-9/]+\s?集\]|\[全\s?[0-9/]+\s?集\])/);
     if (result === null) return false;
-    return targetConf.list.some(drama => 
+    return targetConf.list.some(drama =>
       drama.name === result[2] && group.includes(drama.group)
     )
   });
@@ -41,8 +46,8 @@ const enterProcessor = ($) => {
 }
 // 目标页面获取 torrent 下载链接
 const productProcessor = ($, name) => {
-  const config = targetConf.list.find(item => item.name === name);
-  const titleDom = $("div[id^='message_'] p:first-child").filter(function() {
+  const config = getConfig(name);
+  const titleDom = $("div[id^='message_'] p:first-child").filter(function () {
     return $(this).text().includes(config.group)
   })
   const aTag = titleDom.first().parents('.post').find('tr td a').last();
@@ -51,52 +56,81 @@ const productProcessor = ($, name) => {
   const result = torrent.match(/attach\-dialog\-fid\-([0-9]+)\-aid\-([0-9]+)\.htm/);
   const aid = result[2];
   if (config.id !== aid) {
-    return { uri: targetConf.host + `attach-download-fid-${result[1]}-aid-${result[2]}.htm`, filename };
+    return { 
+      uri: targetConf.host + `attach-download-fid-${result[1]}-aid-${result[2]}.htm`, 
+      filename,
+      id: aid,
+    };
   } else {
     return false;
   }
 }
 var download = new Crawler({
-  encoding:null,
-  jQuery:false,// set false to suppress warning message.
-  callback:function(err, res, done){
-      if(err){
-          console.error(err.stack);
-      }else{
-          fs.createWriteStream(res.options.filename).write(res.body);
+  encoding: null,
+  jQuery: false,// set false to suppress warning message.
+  callback: function (err, res, done) {
+    if (err) {
+      console.error(err.stack);
+    } else {
+      const torrentPath = path.join(__dirname, './torrent');
+      try {
+        fs.mkdirSync(torrentPath);
+      } catch (e) { 
+        if (e.code !== 'EEXIST')
+        console.error(e)
       }
-      
-      done();
+      const filePath = path.join(torrentPath, res.options.dname);
+      try {
+        fs.mkdirSync(filePath);
+      } catch(e) {
+        if (e.code !== 'EEXIST')
+        console.error(e)
+      }
+      const file = path.join(filePath, res.options.filename);
+      try {
+        fs.createWriteStream(file).write(res.body);
+        const config = getConfig(res.options.dname);
+        config.id = res.options.id;
+      }catch(e) {
+        console.error(e);
+      }
+    }
+
+    done();
   }
 });
+download.on('drain', () => {
+  const yamlStr = YAML.stringify(targetConf);
+  fs.writeFileSync('./target.yml', yamlStr);
+})
 
 // const torrent = new Crawler()
 const product = new Crawler({
-  maxConnections : 10,
+  maxConnections: 10,
   // This will be called for each crawled page
-  callback : function (error, res, done) {
-      if(error){
-          console.log(error);
-      }else{
-          var $ = res.$;
-          const options = productProcessor($, res.options.name);
-          download.queue(options)
-      }
-      done();
+  callback: function (error, res, done) {
+    if (error) {
+      console.log(error);
+    } else {
+      var $ = res.$;
+      const options = productProcessor($, res.options.name);
+      download.queue({ ...options, dname: res.options.name })
+    }
+    done();
   }
 })
 const enter = new Crawler({
-  maxConnections : 10,
+  maxConnections: 10,
   // This will be called for each crawled page
-  callback : function (error, res, done) {
-      if(error){
-          console.log(error);
-      }else{
-          var $ = res.$;
-          tmpList = enterProcessor($);
-          tmpList.forEach(item => product.queue({ uri: item.url, name: item.name }))
-      }
-      done();
+  callback: function (error, res, done) {
+    if (error) {
+      console.log(error);
+    } else {
+      var $ = res.$;
+      tmpList = enterProcessor($);
+      tmpList.forEach(item => product.queue({ uri: item.url, name: item.name }))
+    }
+    done();
   }
 })
 
